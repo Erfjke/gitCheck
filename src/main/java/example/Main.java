68fbe4310144d22cc1c5b8f7834c30a7698b728e
Nazar <nazar.vasilkov2005@gmail.com>
2024-09-11 20:57:20 +0300

    2

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -10,8 +10,7 @@ public class Main {
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            System.out.println("do smth");
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
