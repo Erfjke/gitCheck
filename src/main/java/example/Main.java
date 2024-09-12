9b906a1cb0c2d1dcc1ae0f2f2ecfc340c717bc04
Nazar <nazar.vasilkov2005@gmail.com>
2024-09-12 15:51:21 +0300

    release branch 2

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -22,6 +22,7 @@ public class Main {
             System.out.println("major feature 2");
             System.out.println("major feature 3");
             System.out.println("release branch");
+            System.out.println("release branch 2");
         }
     }
 }
