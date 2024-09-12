30f290ae3f37d40173a988ae133c8a7d9fdfe292
Nazar <nazar.vasilkov2005@gmail.com>
2024-09-12 16:08:20 +0300

    smth

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -24,6 +24,7 @@ public class Main {
             System.out.println("release branch");
             System.out.println("release branch 2");
             System.out.println("only bugfixes");
+            System.out.println("smth");
         }
     }
 }
